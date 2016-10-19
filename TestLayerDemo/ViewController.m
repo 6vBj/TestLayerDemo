2749759c3d557340d975223e1fd0d8583ac51708
6vBj <hbliuwei123@gmail.com>
2016-10-19 17:21:30 +0800

    remember

--- TestLayerDemo/ViewController.m
+++ TestLayerDemo/ViewController.m
@@ -17,6 +17,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    CATextLayer * textLayer = [[CATextLayer alloc]init];
+    textLayer.frame = self.view.bounds;
+    
+    textLayer.foregroundColor = [UIColor redColor].CGColor;
+    textLayer.backgroundColor = [UIColor yellowColor].CGColor;
+    textLayer.alignmentMode = kCAAlignmentNatural;
+    textLayer.wrapped = YES;
+    //避免变虚
+    textLayer.contentsScale = [UIScreen mainScreen].scale;
+   
+    
+    UIFont * font = [UIFont systemFontOfSize:25];
+    CFStringRef fontName = (__bridge CFStringRef)font.fontName;
+    CGFontRef fontRef = CGFontCreateWithFontName(fontName);
+    textLayer.font = fontRef;
+    textLayer.fontSize = font.pointSize;
+    CGFontRelease(fontRef);
+    
+    NSString * textStr = @"avnsklvhsobvndkjbvndbnvdkfbvndfjkbndbnkdjbfnvkdjbfnkjbnkdjfbndjkbn";
+    //可以用string，也可以用 attributedString
+    textLayer.string = textStr;
+    
+    [self.view.layer addSublayer:textLayer];
 }
 
 - (void)didReceiveMemoryWarning {
